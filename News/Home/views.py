35376a38b2f66714f4cc628fa3d7a139a0099be0
GAURAV PONKIYA <39650633+GP1999@users.noreply.github.com>
2019-11-07 16:34:33 +0530

    Update views.py

--- News/Home/views.py
+++ News/Home/views.py
@@ -8,14 +8,14 @@ imageurls=[]
 def index(request):
     url=""
     if request.method=='GET':
-        url=('https://newsapi.org/v2/top-headlines?''country=in&''apiKey=2c653aaad7e74b73ad5160b2be0cbbde')
+        url=('https://newsapi.org/v2/top-headlines?''country=in&''apiKey=API_KEY')
     else:
         country=request.POST['country']
         category=request.POST['category']
         if type(country)!=type("none") or type(category) != type("none") :
-            url=('https://newsapi.org/v2/top-headlines?''country=in&''apiKey=2c653aaad7e74b73ad5160b2be0cbbde')
+            url=('https://newsapi.org/v2/top-headlines?''country=in&''apiKey=API_KEY')
         else:    
-            url='https://newsapi.org/v2/top-headlines?'+'country='+country+'&'+'category='+category+'&'+'apiKey=2c653aaad7e74b73ad5160b2be0cbbde'
+            url='https://newsapi.org/v2/top-headlines?'+'country='+country+'&'+'category='+category+'&'+'apiKey=API_KEY'
 
     response=requests.get(url)
     Articles=response.json()['articles'];  
@@ -30,20 +30,3 @@ def index(request):
             discript.append(sp['description'])
     return render(request,'Home/index.html',{'headings':head,'articles':discript,'imglinks':imageurls,'urls':urls})
 
-# def articles(request,articlenumber):
-#     return render(request,'Home/Article.html',{'article':discript[articlenumber],'links':urls[articlenumber],'imglinks':imageurls[articlenumber]})
-
-# /*def filter(request):
-#     if request.method=='POST':
-#         country=request.POST['country']
-#         category=request.POST['category']
-#         head=[]
-#         discript=[]
-#         urls=[]
-#         imageurls=[]
-        
-#         url='https://newsapi.org/v2/top-headlines?'+'country='+country+'&'+'category='+category+'&'+'apiKey=2c653aaad7e74b73ad5160b2be0cbbde'
-#         response=requests.get(url)
-#         return render(request,'Home/index.html',{'headings':head,'articles':discript,'imglinks':imageurls,'urls':urls})
-
-# */
